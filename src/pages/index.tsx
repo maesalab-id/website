40bb412d395cbbd56645d00324fe0397a4861f5f
Imanuel Pundoko <ilomon10@gmail.com>
2022-04-19 23:12:01 +0800

    Update open graph image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { Header } from '../components/Home/Header'
 import { Footer } from '../components/Home/Footer'
 import { HeroBanner } from '../components/Home/HeroBanner'
@@ -9,6 +8,7 @@ import { WhyMaesa } from '../components/Home/WhyMaesa'
 import { SecondHeroBanner } from '../components/Home/SecondHeroBanner'
 import { Products } from '../components/Home/Products'
 import { Services } from '../components/Home/Services'
+import Favicon from '../../public/favicon.svg'
 
 const Home: NextPage = () => {
   return (
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
         <meta property="og:type" content="article" key="ogtype" />
         <meta property="og:title" content={"Maesalab Tech Indonesia"} key="ogtitle" />
         <meta property="og:description" content={`Focused on Web Standards to build better technologies`} key="ogdesc" />
-        <meta property="og:image" content={"/favicon.ico"} key="ogimage" />
+        <meta property="og:image" content={Favicon} key="ogimage" />
         <meta property="og:image:width" content={"512"} key="ogimagewidth" />
         <meta property="og:image:height" content={"512"} key="ogimageheight" />
         <meta property="og:site_name" content="Maesalab" key="ogsitename" />

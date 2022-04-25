c948ac49f6ea0b098587e5c38479afb00c948680
Imanuel Pundoko <ilomon10@gmail.com>
2022-04-25 23:08:24 +0800

    Add more product on hero slider

--- src/components/Home/HeroBanner.tsx
+++ src/components/Home/HeroBanner.tsx
@@ -1,22 +1,50 @@
 import React, { useMemo } from 'react';
 import { Container, Text, Button, Group, useMantineTheme, Title, List, ThemeIcon, Image, AspectRatio, Badge } from '@mantine/core';
 import useStyles from './HeroBanner.styles';
-import { IconCheck } from '@tabler/icons';
-import slider_ovor from "./ovor.png";
-import slider_pos from "./pos.png";
 import { NextLink } from '@mantine/next';
 import Slider from "react-slick";
 import { Window } from './Window';
+
+import slider_akademik from "./assets/akademik.png";
+import slider_digisign from "./assets/digisign.jpg";
+import slider_easpirasi from "./assets/easpirasi.png";
+import slider_elan from "./assets/elan.jpg";
+import slider_eoffice from "./assets/eoffice.png";
+import slider_jobdesk from "./assets/jobdesk.jpg";
+import slider_ovor from "./assets/ovor.png";
+import slider_pos from "./assets/pos.png";
+import slider_website from "./assets/website.jpg";
 
 export function HeroBanner() {
 
   const { classes } = useStyles();
   const slider = useMemo(() => [{
+    image: slider_akademik,
+    label: "https://akademik.maesalab.com"
+  }, {
+    image: slider_digisign,
+    label: "https://digisign.maesalab.com"
+  }, {
+    image: slider_easpirasi,
+    label: "https://easpirasi.maesalab.com"
+  }, {
+    image: slider_elan,
+    label: "https://elan.maesalab.com"
+  }, {
+    image: slider_eoffice,
+    label: "https://eoffice.maesalab.com"
+  }, {
+    image: slider_jobdesk,
+    label: "https://jobdesk.maesalab.com"
+  }, {
     image: slider_ovor,
     label: "https://ovor.maesalab.com"
   }, {
     image: slider_pos,
     label: "https://pos.maesalab.com"
+  }, {
+    image: slider_website,
+    label: "https://website.maesalab.com"
   }], []);
   return (
     <Container size={"lg"}>
@@ -44,7 +72,7 @@ export function HeroBanner() {
             // infinite
             arrows={false}
           >
-            {slider.map(({ image, label}, index) => (
+            {slider.map(({ image, label }, index) => (
               <div key={index} className={classes.image} >
                 <Window title={label}>
                   <AspectRatio ratio={16 / 9}>
